43a6744fa7c4741f58c218bc4be8ae8d5c1baeb9
DesG01211517 <desmongarrison2@gmail.com>
2024-03-06 20:48:58 -0500

    complete

--- index.js
+++ index.js
@@ -7,65 +7,65 @@
 // Remember that i++ mean "increment"
 // it takes the variable i up by 1.
 
-let i = 0;
+//let i = 0;
 
-console.log("**** Incrementing i ****");
-console.log(`i starts as ${i}`);
+//console.log("**** Incrementing i ****");
+//console.log(`i starts as ${i}`);
 
-i++;
-console.log(`i++ brings i to ${i}`);
+//i++;
+//console.log(`i++ brings i to ${i}`);
 
-i++;
+//i++;
 
-console.log(`i++, again, brings i to ${i}`);
-i++;
+//console.log(`i++, again, brings i to ${i}`);
+//i++;
 
 // TODO: increment i to 3
-console.log(`i++, again, brings i to ${i}`);
-i++
+//console.log(`i++, again, brings i to ${i}`);
+//i++
 
 
 // TODO: increment i to 4
-console.log(`i++, again, brings i to ${i}`);
-i++
+//console.log(`i++, again, brings i to ${i}`);
+//i++
 // TODO: increment i to 5
-console.log(`i++, again, brings i to ${i}`);
-i++
+//console.log(`i++, again, brings i to ${i}`);
+//i++
 // TODO: increment i to 10.
 // Then log out that value to the console.
-for (let t = 0; t<11; t++){
-  console.log(t);
-}
+//for (let t = 0; t<11; t++){
+//  console.log(t);
+//}
 //let i = 0;
 //for (let i=0; i<11; i++){
 //  console.log
 
 // Part 2
 // For loops can take care of this in just 3 lines!
-console.log("**** Introducing the for-loop! ****");
+//console.log("**** Introducing the for-loop! ****");
 // TODO: change the for loop to iterate up to 10.
-for (let i = 0; i < 11; i++) {
-  console.log(`Now, i is ${i}.`);
-}
+//for (let i = 0; i < 11; i++) {
+//  console.log(`Now, i is ${i}.`);
+//}
 
 // TODO: make a for loop that counts up to 25.
-for (let d = 0; d<26; d++){
-  console.log(d);
-}
+//for (let d = 0; d<26; d++){
+//  console.log(d);
+//}
 // TODO: make a for loop that counts up to 50.
-for (let z = 0; z<51; z++){
-  console.log(z);
-}
+//for (let z = 0; z<51; z++){
+//  console.log(z);
+//}
 
 // TODO: make a for loop that counts up to 100.
-for (let y = 0; y<101; y++){
-  console.log(y);
-}
+//for (let y = 0; y<101; y++){
+//  console.log(y);
+//}
 
 // TODO: make a for loop that counts up to 1001.
-for (let d = 0; d<1002; d++){
-  console.log(d);
-}
+//for (let d = 0; d<1002; d++){
+//  console.log(d);
+//}
 
 // TODO: Add, Commit, Push!
 // TODO: Comment out the above so that they don't bother you as you do the rest of the assignment.
@@ -111,12 +111,26 @@ let animals = ["aardvark", "buffalo", "cat", "dingo"];
 console.log("**** Animals ****");
 // Let use console.log to print each item in order.
 for (let i = 0; i < 4; i++) {
-  console.log(animals[i]);
+  console.log(animals[0]);
+  console.log(`${animals[0]}`)
+  console.log(`${animals[1]}`)
+  console.log(`${animals[2]}`)
+  console.log(`${animals[3]}`)
 }
-
 // TODO: make a new array of zooAnimals. Use a loop to print them all to the console.
 // Bonus: Can you think of a use for array.length in the for loop?
+let zooAnimals = ["zebra", "lion", "tiger", "shark"];
+for (let z = 0; z<4; z++){
+  console.log(zooAnimals[z])
 
+}
+//console.log(`A ${zooAnimal[0]} lives in the zoo`);
+//z++
+//console.log(`A ${zooAnimal[1]} lives in the zoo`);
+//z++
+//console.log(`A ${zooAnimal[2]} lives in the zoo`);
+//z++
+//console.log(`A ${zooAnimal[3]} lives in the zoo`);
 // TODO: make a new array of sports teams. Use a loop to print them all to the console.
 // Bonus: Can you think of a use for array.length in the for loop?
 
@@ -137,6 +151,10 @@ console.log(animals);
 // so all the values are "-".
 let board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
 console.log(board);
+for (let i = 0; i < 9; i++){
+  board[i] = "-";
+}
+console.log(board);
 
 // TODO: Use a for loop to reset the battlship board so all the values are open ocean. ("o")
 board = [
@@ -145,6 +163,10 @@ board = [
   "S", "C", "C", "C",
   "S", "o", "o", "o",
 ];
+console.log(board);
+for (let i = 0; i<17; i++){
+  board[i] = "o"
+}
 console.log(board);
 
 // TODO: Add, Commit, Push!
@@ -157,6 +179,13 @@ board = [
   "S", "C", "C", "C",
   "S", "o", "o", "o",
 ];
+for (let i = 0; i<4; i++){
+  if (board[i] === "D") {
+    board[i] = "o";
+  }
+}
+console.log(board);
+
 
 // Example: We can use a while loop in a for loop.
 // Just nest them and make sure you
@@ -210,9 +239,15 @@ board = [
 
 console.log("**** Setting specific items ****");
 for (let i = 0; i < board.length; i++) {
-  if (board[i] === "S") {
+  if (board[i] === "D") {
     board[i] = "X";
   }
+  if (board[i] === "S") {
+    board[i] = "X"
+  }
+  if (board[i] === "C") [
+    board[i] = "X"
+  ]
 }
 console.log(board);
 
